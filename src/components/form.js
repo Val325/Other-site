98186e505f3a7d5e9db489632b847dd448efc23a
Val325 <valera777pushkin777@gmail.com>
2023-06-29 21:31:15 +0300

    small fixes for download images

--- src/components/form.js
+++ src/components/form.js
@@ -6,29 +6,32 @@ function Form() {
   const [isSent, setIsSent] = useState(false);
   const [msg, setMsg] = useState('');
   const [Posts, setPosts] = useState([{}]);
+  const [image, setImage] = useState({ data: '' });
+
   function handleSubmit() {
       // send to server
-	
+      setPosts([...Posts,{"text":msg}]) 
+      const formData = new FormData();
+      formData.append('image', image);
+      formData.append('text', msg);
+      
       fetch('http://127.0.0.1:8000/', {
         method: 'POST',
-        headers: {
-           'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          data: {
-             text: msg,
-             
-           }
-         })
+	
+        body:formData 
       });
 
-      setPosts([...Posts,{"text":msg}]) 
-  } 
+      //setPosts([...Posts,{"text":msg}]) 
+  }
+
+
+
   return (
     <div className="Form">
-      <form method="post" action="http://127.0.0.1:8000/"> 
+      <form method="post" action="http://127.0.0.1:8000/" encType="multipart/form-data"> 
         <label for="textPost">Text</label><br />
         <input type="text" onChange={e => setMsg(e.target.value)} value={msg} /><br /><br />
+	<input type="file" name="image" onChange={e => setImage(e.target.files[0])} /><br />
 	<input type="button" className="ShowForm" value="Send" onClick={handleSubmit} />
       </form>
       
