295939824cf052a61b6057e80447329c6428f581
Val325 <valera777pushkin777@gmail.com>
2023-06-29 21:31:53 +0300

    download image

--- node/server.js
+++ node/server.js
@@ -8,6 +8,9 @@ const db = new sqlite3.Database('mydb.db');
 const express = require("express");
 const cors = require('cors')
 const app = express();
+const multer  = require('multer')
+//const upload = multer({ dest: 'uploads/' })
+
 
 app.use(cors({
     origin: '*'
@@ -15,6 +18,19 @@ app.use(cors({
 
 app.use(express.urlencoded());
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
+
 app.get('/', function(req, res) {
   let data_db = {};
   
@@ -35,19 +51,22 @@ app.get('/', function(req, res) {
 
 
 })
-app.post('/', function(request, response){
-    console.log("From frontend: ", request.body.data.text);
+app.post('/',upload.single('image'), function(request, response){
+    console.log("From frontend: ", request.body.text);
+    console.log("Image from frontend: ", request.file, request.body);
+
     db.serialize(function() {
     // Create a table
     db.run("CREATE TABLE IF NOT EXISTS TextTables (id INTEGER PRIMARY KEY, datatext TEXT)");
 
     // Insert data into the table
-    db.run("INSERT INTO TextTables (datatext) VALUES (?)", request.body.data.text);
+    db.run("INSERT INTO TextTables (datatext) VALUES (?)", request.body.text);
 
     
    });
    //db.close(); 
 })
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
